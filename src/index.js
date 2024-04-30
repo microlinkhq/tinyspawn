ac424c090f0c81cef4ed1f519821eddf3e33ab6a
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-04-30 11:07:14 +0200

    fix: expose kill, ref & unref methods (#11)
    * fix: expose kill, ref & unref methods
    
    closes #10
    
    * ci: remove node16
    
    * ci: avoid npx
    
    it's failing in windows

--- src/index.js
+++ src/index.js
@@ -3,19 +3,29 @@
 const { spawn } = require('child_process')
 const { EOL } = require('os')
 
-const EE_PROPS = Object.getOwnPropertyNames(require('events').EventEmitter.prototype).filter(name => !name.startsWith('_'))
+const EE_PROPS = Object.getOwnPropertyNames(
+  require('events').EventEmitter.prototype
+)
+  .filter(name => !name.startsWith('_'))
+  .concat(['kill', 'ref', 'unref'])
 
-const eos = (stream, listener, buffer = []) => stream[listener].on('data', data => buffer.push(data)) && buffer
+const eos = (stream, listener, buffer = []) =>
+  stream[listener].on('data', data => buffer.push(data)) && buffer
 
 const clean = str => str.trim().replace(/\n$/, '')
 
-const parse = (buffer, { json } = {}) => (encoding, start, end) => {
-  const data = clean(Buffer.concat(buffer).toString(encoding, start, end))
-  return json ? JSON.parse(data) : data
-}
+const parse =
+  (buffer, { json } = {}) =>
+    (encoding, start, end) => {
+      const data = clean(Buffer.concat(buffer).toString(encoding, start, end))
+      return json ? JSON.parse(data) : data
+    }
 
 const extend = defaults => (input, args, options) => {
-  if (!(args instanceof Array)) { options = args; args = [] }
+  if (!(args instanceof Array)) {
+    options = args
+    args = []
+  }
   const [cmd, ...cmdArgs] = input.split(' ').concat(args).filter(Boolean)
   let childProcess
 
@@ -25,27 +35,36 @@ const extend = defaults => (input, args, options) => {
     const stdout = eos(childProcess, 'stdout')
     const stderr = eos(childProcess, 'stderr')
 
-    childProcess
-      .on('error', reject)
-      .on('exit', code => {
-        Object.defineProperty(childProcess, 'stdout', { get: parse(stdout, opts) })
-        Object.defineProperty(childProcess, 'stderr', { get: parse(stderr) })
-        if (code === 0) return resolve(childProcess)
-        const command = `${cmd} ${cmdArgs.join(' ')}`
-        let message = `The command spawned as:${EOL}${EOL}`
-        message += `  ${command}${EOL}${EOL}`
-        message += `exited with \`{ code: ${code} }\` and the following trace:${EOL}${EOL}`
-        message += String(stderr).split(EOL).map(line => `  ${line}`).join(EOL)
-        const error = new Error(message)
-        error.command = command
-        error.name = 'ChildProcessError'
-        Object.keys(childProcess).forEach(key => { error[key] = childProcess[key] })
-        reject(error)
+    childProcess.on('error', reject).on('exit', code => {
+      Object.defineProperty(childProcess, 'stdout', {
+        get: parse(stdout, opts)
       })
+      Object.defineProperty(childProcess, 'stderr', { get: parse(stderr) })
+      if (code === 0) return resolve(childProcess)
+      const command = `${cmd} ${cmdArgs.join(' ')}`
+      let message = `The command spawned as:${EOL}${EOL}`
+      message += `  ${command}${EOL}${EOL}`
+      message += `exited with \`{ code: ${code} }\` and the following trace:${EOL}${EOL}`
+      message += String(stderr)
+        .split(EOL)
+        .map(line => `  ${line}`)
+        .join(EOL)
+      const error = new Error(message)
+      error.command = command
+      error.name = 'ChildProcessError'
+      Object.keys(childProcess).forEach(key => {
+        error[key] = childProcess[key]
+      })
+      reject(error)
+    })
   })
 
   const subprocess = Object.assign(promise, childProcess)
-  if (childProcess) EE_PROPS.forEach(name => (subprocess[name] = childProcess[name].bind(childProcess)))
+  if (childProcess) {
+    EE_PROPS.forEach(
+      name => (subprocess[name] = childProcess[name].bind(childProcess))
+    )
+  }
   return subprocess
 }
 
