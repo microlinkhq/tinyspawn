628e7534f552b43c718beac8063f30abb37b565c
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-06-19 12:05:01 +0200

    feat: add TypeScript types (#15)

---
+++ src/index.d.ts
@@ -0,0 +1,21 @@
+import { ChildProcess, SpawnOptions } from 'child_process';
+
+interface ExtendOptions extends SpawnOptions {
+  json?: boolean;
+}
+
+interface ResolvedSubprocess extends Omit<ChildProcess, 'stdout' | 'stderr'> {
+  stdout: string;
+  stderr: string;
+}
+
+interface SubprocessPromise extends Promise<ResolvedSubprocess>, ChildProcess {}
+
+declare function extend(defaults?: ExtendOptions): (input: string, args?: string[] | ExtendOptions, options?: ExtendOptions) => SubprocessPromise;
+
+declare const $: ReturnType<typeof extend> & {
+  extend: typeof extend;
+  json: ReturnType<typeof extend>;
+};
+
+export = $;
